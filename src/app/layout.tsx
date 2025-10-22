22119391edaa63f22844f1f2ee70fe5f8b54ca9c
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-10-22 17:19:45 +0900

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} container flex w-full justify-center bg-emerald-300`}
       >
         {children}
       </body>
